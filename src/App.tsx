7957237b060f1b3136d1c7e5d8f1afb9aa9c87e2
Josh Campbell <josh@p4a.net>
2022-09-02 16:07:29 -0700

    just the beginning

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { AppContainer, CardContainer, ColumnContainer, ColumnTitle } from "./styles"
+
 export const App = () => {
-  return null
+  return <AppContainer>
+    <ColumnContainer>
+      <ColumnTitle>ToDo</ColumnTitle>
+      <CardContainer>One</CardContainer>
+      <CardContainer>Two</CardContainer>
+      <CardContainer>Three</CardContainer>
+    </ColumnContainer>
+  </AppContainer>
 }
